eaa382317fc5ab14162b125c9929d28cda9fbe18
joychae <sooie35@gmail.com>
2021-03-20 23:23:41 +0900

    Update bj1904.py

--- bj1904.py
+++ bj1904.py
@@ -5,26 +5,26 @@ dp = [0]*1000001
 dp[1] = 1
 dp[2] = 2
 for i in range(3,n+1):
-    dp[i] = ((dp[i-1] % 15746) + (dp[i-2] % 15746))%15746
+    dp[i] = ((dp[i-1] % 15746) + (dp[i-2] % 15746)) %15746
 print(dp[n])
 
 # # DP를 위한 저장소 생성
-# import sys
+import sys
 
-# memo = {
-#     0: 1,
-#     1: 2
-# }
+memo = {
+    0: 1,
+    1: 2
+}
 
-# # DP 함수식 구현
-# def dynamic_programming(n, fibo_memo):
-#     if n in fibo_memo:
-#         return fibo_memo[n]
+# DP 함수식 구현
+def dynamic_programming(n, fibo_memo):
+    if n in fibo_memo:
+        return fibo_memo[n]
 
-#     nth_fibo = dynamic_programming(n - 1, fibo_memo) + dynamic_programming(n - 2, fibo_memo)
-#     fibo_memo[n] = nth_fibo
-#     return nth_fibo
+    nth_fibo = dynamic_programming(n - 1, fibo_memo) + dynamic_programming(n - 2, fibo_memo)
+    fibo_memo[n] = nth_fibo
+    return nth_fibo
 
-# n = int(sys.stdin.readline().strip())
+n = int(sys.stdin.readline().strip())
 
-# print(dynamic_programming(n-1,memo))
+print(dynamic_programming(n-1,memo))
