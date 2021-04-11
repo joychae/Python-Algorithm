22eeb63f225b28541f3f86083dc24624e7940e17
joychae <sooie35@gmail.com>
2021-04-11 20:46:08 +0900

    분할정복 복습

--- bj2630.py
+++ bj2630.py
@@ -1,107 +1,34 @@
 # 색종이 만들기 (분할정복 연습)
-# 틀린 문제
+# 1회차 - 틀린 문제, 2회차 - 답안 참고해서 푼 문제 92ms
+# 2회차 - 재귀함수 연습이 더 필요하다
 
 import sys
 
-N = int(sys.stdin.readline().strip())
-paper_list = [list(map(int,sys.stdin.readline().strip().split())) for _ in range(N)]
-white_count = 0
-blue_count = 0
+side = int(sys.stdin.readline().strip())
+color_list = [list(map(int,sys.stdin.readline().strip().split())) for _ in range(side)]
+white_color_cnt = 0
+blue_color_cnt = 0
 
 def divide(x, y, n):
-    global white_count, blue_count
-    paper_color = paper_list[x][y]
-    check_color = True
+    global white_color_cnt, blue_color_cnt
+    paper_color = color_list[x][y]
+    paper_color_check = 1
 
-    for i in range(x,x+n):
-        for j in range(y,y+n):
-            if paper_color != paper_list[i][j]:
-                check_color = False
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if paper_color != color_list[i][j]:
+                paper_color_check = 0
                 divide(x, y, n//2)
-                divide(x, y+(n//2), n//2)
-                divide(x+(n//2), y, n//2)
-                divide(x+(n//2), y+(n//2), n//2)
+                divide(x, y+n//2, n//2)
+                divide(x+n//2, y, n//2)
+                divide(x+n//2, y+n//2, n//2)
                 return
     
-    if check_color:
-        if paper_color:
-            blue_count += 1
-        elif not paper_color:
-            white_count += 1
+    if paper_color:
+        blue_color_cnt += 1
+    elif not paper_color:
+        white_color_cnt += 1
 
-divide(0, 0, N)
-print(white_count)
-print(blue_count)
-
-# def divide(N, paper_list):
-#     left_slice = [row[0:N//2] for row in paper_list]
-#     right_slice = [row[N//2:N+1] for row in paper_list]
-#     paper_1 = left_slice[0:N//2]
-#     paper_2 = right_slice[0:N//2]
-#     paper_3 = left_slice[N//2:N+1]
-#     paper_4 = right_slice[N//2:N+1]
-
-#     return paper_1, paper_2, paper_3, paper_4
-
-# count_blue = 0
-# count_white = 0
-
-# while True:
-#     paper_list_1 = divide(N, paper_list)[0]
-#     # paper_list_2 = divide(N, paper_list)[1]
-#     # paper_list_3 = divide(N, paper_list)[2]
-#     # paper_list_4 = divide(N, paper_list)[3]
-
-#     if 1 and 0 in paper_list_1:
-#         divide(N//2, paper_list_1)
-#     else:
-#         if 1 in paper_list_1:
-#             count_blue += 1
-#             break
-#         elif 0 in paper_list_1:
-#             count_white +=1
-#             break
-#     N = N//2
-#     if len(paper_list_1) == 1:
-#         if 1 in paper_list_1:
-#             count_blue +=1
-#         elif 0 in paper_list_1:
-#             count_white += 1
-#         break
-
-# print(paper_list_1)
-# print(count_white, count_blue)
-
-# import sys
-# n=int(sys.stdin.readline())
- 
-# color_paper=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]#x행 y열
- 
-# white=0#0이면 흰생
-# blue=0#1이면 파란색
- 
-# def cut(x,y,n):
-#     global blue,white
-#     check=color_paper[x][y]
-#     for i in range(x,x+n):
-#         for j in range(y,y+n):
-#             if check!=color_paper[i][j]:#하나라도 같은색이 아니라면
-#                 #4등분
-#                 cut(x,y,n//2)#1사분면
-#                 cut(x,y+n//2,n//2)#2사분면
-#                 cut(x+n//2,y,n//2)#3사분면
-#                 cut(x+n//2,y+n//2,n//2)#4사분면
-#                 return
- 
-#     if check==0:#모두 흰색일때
-#         white+=1
-#         return
-#     else:   #모두 파란색일때
-#         blue+=1
-#         return
- 
- 
-# cut(0,0,n)
-# print(white)
-# print(blue)
-
+divide(0, 0 , side)
+print(white_color_cnt)
+print(blue_color_cnt)
